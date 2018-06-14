b79d37dfd8cb28c5109278beecce67bb8d28b66a
gangeshraj <gangeshraj1@gmail.com>
2018-06-14 08:00:35 +0530

    it is using http which is not used in angular 6because angular 6 uses httpcclient in future I am going to change it

---
+++ src/app/server.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
+import { map } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators'; 
+import { throwError } from 'rxjs'; 
+
+@Injectable()
+export class ServerService {
+  constructor(private http: Http) {}
+  storeServers(servers: any[]) {
+    const headers = new Headers({'Content-Type': 'application/json'});
+    // return this.http.post('https://udemy-ng-http.firebaseio.com/data.json',
+    //   servers,
+    //   {headers: headers});
+    return this.http.put('https://udemy-ng-http.firebaseio.com/data.json',
+      servers,
+      {headers: headers});
+  }
+  getServers() {
+    return this.http.get('https://udemy-ng-http.firebaseio.com/data')
+      .pipe(map(
+        (response: Response) => {
+          const data = response.json();
+          for (const server of data) {
+            server.name = 'FETCHED_' + server.name;
+          }
+          return data;
+        }
+      ))
+      .pipe(catchError(
+        (error: Response) => {
+          return throwError('Something went wrong');
+        }
+      ));
+  }
+  getAppName() {
+    return this.http.get('https://udemy-ng-http.firebaseio.com/appName.json')
+      .pipe(map(
+        (response: Response) => {
+          return response.json();
+        }
+      ));
+  }
+}
